3e492458c8beb2576dc96f9fbf91b7d53f5b92a2
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-22 09:43:33 +0530

    Add method to retrieve log file from configuration

--- src/server/Config.java
+++ src/server/Config.java
@@ -6,19 +6,23 @@ import java.util.Properties;
 
 public class Config {
     private final Properties properties;
-
+    
     public Config(String configFile) throws IOException {
         properties = new Properties();
-        try(FileInputStream fis = new FileInputStream(configFile)) {
+        try (FileInputStream fis = new FileInputStream(configFile)) {
             properties.load(fis);
         }
     }
-
+    
     public int getPort() {
-        return Integer.parseInt(properties.getProperty("port","8080"));
+        return Integer.parseInt(properties.getProperty("port", "8080"));
     }
-
+    
     public String getStaticDir() {
         return properties.getProperty("static.dir", "static");
+    }
+    
+    public String getLogFile() {
+        return properties.getProperty("log.file", "server.log");
     }
 }
