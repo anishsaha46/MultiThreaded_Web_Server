3ffa28b84f4a3c21580c26c986f5863bc862ab5f
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-22 10:06:15 +0530

    Add WebServer class skeleton for handling server operations

---
+++ src/server/WebServer.java
@@ -0,0 +1,15 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+
+public class WebServer {
+    
+}
