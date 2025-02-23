bfdc296b4173f03a363931be916d466823c2aa1a
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-23 10:40:37 +0530

    Add constructor to RequestHandler for initializing client socket, router, logger, and config

--- src/server/RequestHandler.java
+++ src/server/RequestHandler.java
@@ -10,5 +10,12 @@ public class RequestHandler implements Runnable {
     private final Logger logger;
     private final Config config;
     private static final int MAX_REQUEST_SIZE = 8192;
-    
+
+
+    public RequestHandler(Socket clientSocket, Router router, Logger logger, Config config) {
+        this.clientSocket = clientSocket;
+        this.router = router;
+        this.logger = logger;
+        this.config = config;
+    }
 }
