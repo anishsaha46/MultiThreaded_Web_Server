022ab09fa212e2aecc3f83af4e8b158e97473d60
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-23 10:46:50 +0530

    Implement run method in RequestHandler to handle client requests and manage input/output streams

--- src/server/RequestHandler.java
+++ src/server/RequestHandler.java
@@ -18,4 +18,38 @@ public class RequestHandler implements Runnable {
         this.logger = logger;
         this.config = config;
     }
+
+    @Override
+    public void run(){
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+
+            BufferedOutputStream dataOut = new BufferedOutputStream(clientSocket.getOutputStream());
+
+            clientSocket.setSoTimeout(5000);
+
+            StringBuilder requestBuilder = new StringBuilder();
+
+            String line;
+            int contentLength = 0;
+
+            // Read Header
+            while((line = in.readLine()) != null && !line.isEmpty()){
+                requestBuilder.append(line).append("\r\n");
+                if(line.startsWith("Content-Length:")){
+                    contentLength = Integer.parseInt(line.split(":")[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            logger.severe("Error handling request: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                logger.severe("Error closing client socket: " + e.getMessage());
+            }
+        }
+    }
 }
